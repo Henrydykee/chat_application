88255a370b8715a2cb250bceb9d0414d48f76fc2
Ugochukwu <dykeehenry@gmail.com>
2020-11-08 23:15:44 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+
+
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+var messages = [
+    {name: "Tim", message: "hello"},
+    {name: "Henry", message: "gwen"}
+];
+
+app.get('/messages',(req,res)=>{
+    res.send(messages)
+})
+
+app.post('/messages',(req,res)=>{
+    messages.push(req.body);
+    res.sendStatus(200)
+})
+
+
+var server = app.listen(3000, () => {
+    console.log("server is listening on port",server.address().port);
+});
